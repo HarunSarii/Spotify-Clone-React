32ae60d9781211f1b5ee764885dc5d8aa4203eee
HarunSarii <harunsari14@gmail.com>
2021-08-17 12:27:58 +0300

    add spotify-web-api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import Login from './components/Login';
+import { getTokenFromUrl } from './components/spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+import Player from './components/Player';
+
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = ''
+    const _token = hash.access_token
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log('person:', user)
+      })
+    }
+    console.log('Token:', _token)
+
+  }, [])
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <Player />
+        ) : (
+          <Login />
+        )
+      }
     </div>
   );
 }
