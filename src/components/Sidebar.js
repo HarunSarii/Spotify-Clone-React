4e0b87dd20eafcc2faee8a0b8051dde70978b9b8
HarunSarii <harunsari14@gmail.com>
2021-08-21 17:17:36 +0300

    update footer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,8 +8,8 @@ import { useDataLayerValue } from './DataLayer';
 
 
 function Sidebar() {
-    const [{ playlists }, dispach] = useDataLayerValue();
-    return (
+    const [{ playlists }, dispatch] = useDataLayerValue();
+    console.log('PlayLISTs:', playlists); return (
         <div className='sidebar'>
             <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt="spotify-logo" />
