feac38b55b3abb60cb307c132949e6e5537fffba
HarunSarii <harunsari14@gmail.com>
2021-08-21 23:49:21 +0300

    add body content and css

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,15 @@ function Body({ spotify }) {
     return (
         <div className='body'>
             <Header spotify={spotify} />
+            <div className="body__info">
+                <img src="" alt="weekly-discover" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>discover weekly</h2>
+                    <p>description</p>
+
+                </div>
+            </div>
         </div>
     )
 }
